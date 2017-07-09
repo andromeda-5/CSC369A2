5f53eaa963e84a6be80ed6d1eb2d8d3efe1b35ac
leirion <lily.aliyeva@mail.utoronto.ca>
2017-07-09 01:10:33 -0400

    implemented clock replacement algorithm

--- clock.c
+++ clock.c
@@ -12,14 +12,29 @@ extern int debug;
 
 extern struct frame *coremap;
 
+int clock_hand;
 /* Page to evict is chosen using the clock algorithm.
  * Returns the page frame number (which is also the index in the coremap)
  * for the page that is to be evicted.
  */
 
 int clock_evict() {
-	
-	return 0;
+	int evict_frame;
+
+	while(true) {
+		pgtbl_entry_t *pte = coremap[clock_hand].pte;
+		if (pte->frame & PG_REF) {
+			pte->frame &= ~PG_REF;
+			clock_hand++;
+			if (clock_hand == memsize){
+				clock_hand = 0;
+			}
+		} else {
+			evict_frame = clock_hand;
+			break; 
+		}
+	}
+	return evict_frame;
 }
 
 /* This function is called on each access to a page to update any information
@@ -27,7 +42,7 @@ int clock_evict() {
  * Input: The page table entry for the page that is being accessed.
  */
 void clock_ref(pgtbl_entry_t *p) {
-
+	p->frame |= PG_REF;
 	return;
 }
 
@@ -35,4 +50,5 @@ void clock_ref(pgtbl_entry_t *p) {
  * algorithm. 
  */
 void clock_init() {
+	clock_hand = 0;
 }
